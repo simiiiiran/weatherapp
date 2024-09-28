389b65ab828d0628a8143c0a301c7c6c29044945
simiiiiran <simranshaw333@gmail.com>
2024-09-28 14:17:32 +0530

    naya mal

--- weather/script.js
+++ weather/script.js
@@ -3,12 +3,13 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather_icon")
+const weatherIcon = document.querySelector(".weather_icon");
+const dynbg = "dynamicBackground"; // Assuming this is the ID of your background element
 
 async function checkweather(city) {
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
-
+    const data = await response.json();
+    console.log("notworking");
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
@@ -16,25 +17,46 @@ async function checkweather(city) {
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-    if(data.weather[0].main == "Clouds"){
+    let bgImageUrl = ""; // Variable to hold the background image URL
+
+    if (data.weather[0].main == "Clouds") {
         weatherIcon.src = "images/clouds.png";
-    }
-    else if(data.weather[0].main == "Clear"){
+        bgImageUrl = "./images/cloudpic.jpg";
+        console.log("Clouds");
+    } else if (data.weather[0].main == "Clear") {
         weatherIcon.src = "images/clear.png";
-    }
-    else if(data.weather[0].main == "Rain"){
+        bgImageUrl = "./images/clear_sky.jpg"; // Change this to your clear sky image
+        console.log("Clear");
+    } else if (data.weather[0].main == "Rain") {
         weatherIcon.src = "images/rain.png";
-    }
-    else if(data.weather[0].main == "Drizzle"){
+        console.log("Rain");
+        document.querySelector("#dynbg").style.backgroundImage  = "./images/rain3.jpg";
+    } else if (data.weather[0].main == "Drizzle") {
         weatherIcon.src = "images/drizzle.png";
-    }
-    else if(data.weather[0].main == "Mist"){
+        console.log("Drizzle");
+        bgImageUrl = "./images/drizzle.jpg"; // Change this to your drizzle image
+    } else if (data.weather[0].main == "Mist") {
+        console.log("mist");
         weatherIcon.src = "images/mist.png";
+        bgImageUrl = "./images/mist.jpg"; // Change this to your mist image
+    }else if (data.weather[0].main == "Haze") {
+        weatherIcon.src = "images/clear.png";
+        bgImageUrl = "./images/haze.jpg"; // Change this to your clear sky image
+        console.log("Clear");
     }
 
+    changeBg(bgImageUrl); // Call the function to change the background
+}
+
+function changeBg(bgimage) {
+    //document.getElementById(dynbg).style.backgroundImage = `url('${bgimage}')`;
+    document.querySelector("#dynbg").style.backgroundImage  = `url('${bgimage}')`;
+    document.querySelector("#dynbg").style.width = '100%';
+    document.querySelector("#dynbg").style.backgroundSize = '100%';
+    document.querySelector('#dynbg').style.backgroundRepeat = 'none';
+    
 }
 
 searchBtn.addEventListener("click", () => {
     checkweather(searchBox.value);
-})
-checkweather();
+});
